9c7de9e18a320bc7325fd1ee0e0ffd6dea68c322
oxiEva <oxieva78@gmail.com>
2024-06-16 10:18:12 +0200

    Skeleton project

---
+++ src/test/java/stepdefs/RunCucumberTest.java
@@ -0,0 +1,18 @@
+package stepdefs;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "json:target/surefire-reports/cucumber.json",
+                "html:target/cucumber-html-report.html"
+        },
+        glue = {"stepdefs"},
+        features = {"src/test/resources/"}
+)
+public class RunCucumberTest {
+}
